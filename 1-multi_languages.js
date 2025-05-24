f0161c2f6a3bf5ba99765e15772d28a7b3dc492b
salahudeen abdulkareem <salayo4real@gmail.com>
2025-05-24 23:03:07 +0100

    new letter added

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -2,6 +2,6 @@ let line1='C is fun';
 console.log(line1);
 let line2 = 'Python is cool';
 console.log(line2);
-let line3 = 'Javascript is amazing';
+let line3 = 'JavaScript is amazing';
 console.log(line3)
 
