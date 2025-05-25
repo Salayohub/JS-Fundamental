19c276b32430421ca2d2bfd957ceaad5fabca5ad
salahudeen abdulkareem <salayo4real@gmail.com>
2025-05-25 16:35:34 +0100

    Updated multi_languages_loop script

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing', 'yoruba is there father'];
 
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
