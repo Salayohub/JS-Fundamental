08c77cb40c4bebf9abd4e0c10580893008ba4fe0
salahudeen abdulkareem <salayo4real@gmail.com>
2025-05-25 09:11:33 +0100

    new example

--- 4-concat.js
+++ 4-concat.js
@@ -2,7 +2,7 @@
 const concat = ['confirm','and cool'];
 
 if (concat[0] && concat[1]) {
-    console.log(`${concat[0]} python is fun ${concat[1]}`);
+    console.log(`${concat[0]} python is  ${concat[1]}`);
 } else {
     console.log("0 argument");
 }
