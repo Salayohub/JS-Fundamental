b84e3e0799edba0e6b2fc55ed9619c1895ec519f
salahudeen abdulkareem <salayo4real@gmail.com>
2025-05-25 08:25:06 +0100

    new changes

--- 4-concat.js
+++ 4-concat.js
@@ -4,5 +4,5 @@ const concat = process.argv.slice(2);
 if (concat[0] && concat[1]) {
     console.log(`${concat[0]} is ${concat[1]}`);
 } else {
-    console.log("insufficient argument");
+    console.log("0 argument");
 }
